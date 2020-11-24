116305979e76adf544960e2f9f87892b4a6f0eac
moldux-dartus <moldux.dartus+github@gmail.com>
2020-11-24 09:35:32 -0600

    first round

--- src/Machine.js
+++ src/Machine.js
@@ -1,6 +1,7 @@
 class Machine {
   constructor() {
     this.snacks = []
+    this.balance = 0
   }
 
   seeSelections() {
@@ -12,6 +13,40 @@ class Machine {
       throw Error("please do not troll. you cannot stock nothing.")
     }
     this.snacks = inventory
+  }
+  
+  displayBalance() {
+    return this.balance
+  }
+
+  deposit(amount) {
+    switch(amount){
+      case 10:
+      case 20:
+      case 50:
+      case 100:
+      case 500:
+        this.balance+=amount
+        return this.balance
+      default:
+        return ("I'm afraid I cannot accept this")
+    }
+  }
+
+  selectItem(code) {
+    let snackObj = this.snacks
+    if (this.snacks.name.includes(code)) {
+      if(snackObj.price > this.displayBalance() && this.displayBalance() == 0){
+          return "We have that in stock"
+      }
+      else{
+          var discrepancy = snackObj.price - this.displayBalance()
+          return `Your deposit is insufficient.  Please add Rs ${discrepancy} for this item`
+      }
+    }
+    else {
+      return `We don't have that in stock`
+    }
   }
 }
 
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -27,18 +27,6 @@ describe('The vending machine', () => {
     expect(vendingMachine.seeSelections()).toEqual([snack])
   })
 
-  // someone attempts to stock without inventory
-  // it('stocks nothing if there is no inventory passed', () => {
-  //   // setup
-  //   const vendingMachine = new Machine()
-
-  //   // exercise
-  //   vendingMachine.stock()
-
-  //   // assertion
-  //   expect(vendingMachine.seeSelections()).toEqual([])
-  // })
-
   it('displays an error if no inventory comes with stocking', () => {
     // setup
     const vendingMachine = new Machine()
@@ -46,5 +34,66 @@ describe('The vending machine', () => {
 
     // exercise & assert
     expect(() => vendingMachine.stock()).toThrow(displayMessage)
+  })
+
+  it('displays how much money was deposited', () => {
+    // setup
+    const vendingMachine = new Machine()
+
+    // exercise
+    vendingMachine.deposit(990)
+    let stockErrorMessage = "I'm afraid I cannot accept this"
+
+    // assert
+    expect(vendingMachine.deposit(500)).toEqual(vendingMachine.displayBalance())
+    expect(vendingMachine.deposit(990)).toEqual(stockErrorMessage)
+  })
+
+  it('displays the total balance in the machine', () => {
+    // setup
+    const vendingMachine = new Machine()
+    
+    // exercise
+    vendingMachine.deposit(100)
+    vendingMachine.deposit(100)
+    vendingMachine.deposit(100)
+
+    // assert
+    expect(vendingMachine.displayBalance()).toEqual(300)
+  })
+
+  it('it displays whether or not an item is in stock', () => {
+    // setup
+    const vendingMachine = new Machine()
+    var notInStock = "We don't have that in stock"; var inStock = "We have that in stock"
+    const snack = {
+      name: 'macadamia nuts',
+      price: 250
+    }
+
+    // exercise
+    vendingMachine.stock(snack)
+
+    // assert
+    expect(vendingMachine.selectItem('pig feet')).toEqual(notInStock)
+    expect(vendingMachine.selectItem('macadamia nuts')).toEqual(inStock)
+  })
+
+  it('it tells me if I need to give it more money for the supplied item', () => {
+    // setup    
+    const vendingMachine = new Machine()
+    const snack = {
+      name: 'macadamia nuts',
+      price: 250
+    }
+    var notEnough = 'Your deposit is insufficient.  Please add Rs 50 for this item'
+
+    // exercise
+    vendingMachine.stock(snack)
+    vendingMachine.deposit(100)
+    vendingMachine.deposit(100)
+
+    // assert
+    expect(vendingMachine.selectItem('macadamia nuts')).toEqual(notEnough)
   })
 })
